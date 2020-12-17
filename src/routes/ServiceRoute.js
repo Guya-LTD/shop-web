40f0396c9f28010c76d2415656bc2055d4a89161
Simonbelete <simonbelete@gmail.com>
2020-12-17 20:38:31 +0300

    Removing history from route

--- src/routes/ServiceRoute.js
+++ src/routes/ServiceRoute.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createBrowserHistory } from "history";
 import {
     BrowserRouter as Router,
     Switch,
@@ -10,10 +9,8 @@ import Error404Page from 'pages/Error404Page';
 import Error500Page from 'pages/Error500Page';
 import IndexPage from 'pages/IndexPage';
 
-const history = createBrowserHistory();
-
 const ServiceRoute = () => (
-    <Router history={history} basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route exact path='/' component={IndexPage} />
             <Route path="/:locale(en|am)?/error" component={Error500Page} />
